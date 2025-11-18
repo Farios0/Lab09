a08ea2f2c5a2d238ac4e94ac76a6aca3cd3e2474
Geremia Francesco Conca <geremia.conca@studio.unibo.it>
2025-11-18 15:25:35 +0100

    added a javaDoc that wwas missing

--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -6,8 +6,7 @@ import java.util.List;
 import java.util.Objects;
 
 /**
- * 
- *
+ * class that implements the {@link Controller} interface.
  */
 public final class SimpleController implements Controller {
 
